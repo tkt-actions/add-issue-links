9fb70c4a8de3f34ab2a975f68ef6e2497ec8e35d
tktcorporation <tktcorporation.go@gmail.com>
2020-08-02 03:17:51 +0900

    write tests for resolve option

--- src/domain/pullRequest/pullRequestBody/PullRequestBody.spec.ts
+++ src/domain/pullRequest/pullRequestBody/PullRequestBody.spec.ts
@@ -1,29 +1,54 @@
 import { PullRequestBody } from './PullRequestBody';
 import { Position } from '../../position/Position';
+import { Resolve } from '../../resolve/Resolve';
+import { container } from 'tsyringe';
+
+interface IPullRequestBody {
+  value: string;
+  addIntoTop(str: string): void;
+  addIntoBottom(str: string): void;
+  addRelatedIssueSection(
+    issueNumber: number,
+    position: Position,
+    resolve: Resolve,
+  );
+}
 
 describe('PullRequestBody', () => {
+  let pullRequestBody: IPullRequestBody;
+  beforeAll(() => {
+    // pullRequestBody = PullRequestBody("description");
+  });
   it('addIntoTopOfBody', () => {
-    const prb = new PullRequestBody('description');
-    prb.addIntoTop('top');
-    expect(prb.value).toBe('top\n\ndescription');
+    pullRequestBody.addIntoTop('top');
+    expect(pullRequestBody.value).toBe('top\n\ndescription');
   });
 
   it('addIntoBottomOfBody', () => {
-    const prb = new PullRequestBody('description');
-    prb.addIntoBottom('bottom');
-    expect(prb.value).toBe('description\n\nbottom');
+    pullRequestBody.addIntoBottom('bottom');
+    expect(pullRequestBody.value).toBe('description\n\nbottom');
   });
 
   describe('addRelatedIssueNumberToBody', () => {
     it('into top', () => {
-      const prb = new PullRequestBody('description');
-      prb.addRelatedIssueSection(12, Position.top());
-      expect(prb.value).toBe(`# Related Issue\n\n- Resolve #12\n\ndescription`);
+      pullRequestBody.addRelatedIssueSection(
+        12,
+        Position.top(),
+        Resolve.true(),
+      );
+      expect(pullRequestBody.value).toBe(
+        `# Related Issue\n\n- Resolve #12\n\ndescription`,
+      );
     });
     it('into bottom', () => {
-      const prb = new PullRequestBody('description');
-      prb.addRelatedIssueSection(12, Position.bottom());
-      expect(prb.value).toBe(`description\n\n# Related Issue\n\n- Resolve #12`);
+      pullRequestBody.addRelatedIssueSection(
+        12,
+        Position.bottom(),
+        Resolve.false(),
+      );
+      expect(pullRequestBody.value).toBe(
+        `description\n\n# Related Issue\n\n- #12`,
+      );
     });
   });
 });
