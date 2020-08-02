d9ac585a17e0a0c0a6a3be40e4397d2fef83a910
tktcorporation <tktcorporation.go@gmail.com>
2020-08-02 22:15:34 +0900

    add Repository domain

---
+++ src/domain/repository/Repository.ts
@@ -0,0 +1,29 @@
+export class Repository {
+  static build = (str: string) => {
+    const match = Repository.matchFields(str);
+    if (!match) return undefined;
+    const fields = Repository.extractFields(match);
+    if (!fields) return undefined;
+    return new Repository(fields?.username, fields?.repositoryName);
+  };
+  constructor(private username: string, private repositoryName: string) {}
+  createText = () => this.username + '/' + this.repositoryName;
+
+  private static readonly fieldsRegex = /^(.+)\/(.+)$/;
+  private static matchFields = (str: string) =>
+    str.match(Repository.fieldsRegex);
+  private static extractFields = (
+    match: RegExpMatchArray,
+  ): {
+    username: string;
+    repositoryName: string;
+  } | null => {
+    const username = match[1];
+    const repositoryName = match[2];
+    if (!username || !repositoryName) return null;
+    return {
+      username,
+      repositoryName,
+    };
+  };
+}
