a0f270dd33f46fe42369c728578007320e292f18
tktcorporation <tktcorporation.go@gmail.com>
2020-08-05 03:49:53 +0900

    implements pr service for commenting to pr

--- src/application/service/PullRequestRecordService.ts
+++ src/application/service/PullRequestRecordService.ts
@@ -1,31 +1,36 @@
-import { Context } from '@actions/github/lib/context';
 import { PullRequestRepository } from '../repository/PullRequestRepository';
 import { Position } from './../../domain/position/Position';
 import { Resolve } from './../../domain/resolve/Resolve';
 import { IssueLinkSection } from './../../domain/pullRequest/pullRequestBody/issueLinkSection/IssueLinkSection';
 import { IssueLink } from './../../domain/pullRequest/pullRequestBody/issueLinkSection/issueLink/IssueLinkText';
-import { Repository } from 'src/domain/repository/Repository';
+import { Repository } from './../../domain/repository/Repository';
+import { PullRequest } from './../../domain/pullRequest/PullRequest';
 
 export class PullRequestRecordService {
   constructor(private readonly pullRequestRepository: PullRequestRepository) {}
 
+  addRelatedIssueNumberAsComment = async (
+    pullRequest: PullRequest,
+    issueNumber: number,
+    resolve: Resolve,
+    repository: Repository | undefined,
+  ) =>
+    this.pullRequestRepository.createComment(
+      pullRequest,
+      new IssueLinkSection([new IssueLink(issueNumber, resolve, repository)]),
+    );
+
   addRelatedIssueNumberToBody = async (
+    pullRequest: PullRequest,
     issueNumber: number,
     position: Position,
     resolve: Resolve,
     repository: Repository | undefined,
-    context: Context,
   ) => {
-    const { repo, issue } = context;
-    const pr = await this.pullRequestRepository.get(
-      issue.number,
-      repo.owner,
-      repo.repo,
-    );
-    pr.body.addRelatedIssueSection(
+    pullRequest.body.addRelatedIssueSection(
       new IssueLinkSection([new IssueLink(issueNumber, resolve, repository)]),
       position,
     );
-    return await this.pullRequestRepository.update(pr);
+    return await this.pullRequestRepository.update(pullRequest);
   };
 }
